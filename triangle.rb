cd9e8627c76dbf4df0ea6b410fd7b282d6a61cd4
Patrick McCarthy <p.t.mccarthy@gmail.com>
2014-03-05 11:41:22 -0800

    Tweak triangle method to always return

--- triangle.rb
+++ triangle.rb
@@ -26,8 +26,7 @@ def triangle(a, b, c)
 
   return :isosceles if a == b || a == c || b == c
 
-  return :scalene if a != b && a != c && b != c
-
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
